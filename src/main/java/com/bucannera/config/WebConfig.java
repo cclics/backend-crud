d676796604d1d4574495daca4232ae1d80cef793
cclics <cclics@gmail.com>
2025-11-05 13:50:15 -0300

    env file added, application yaml and webconfig updated

--- src/main/java/com/bucannera/config/WebConfig.java
+++ src/main/java/com/bucannera/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.bucannera.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-                //.allowedOrigins("http://localhost") // Allow requests from this origin
-                .allowedOrigins("https://crud-app-car.netlify.app") // Allow requests from this origin
+                //.allowedOrigins("http://localhost")
+                .allowedOrigins(frontendUrl) // Allow requests from this origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
                 .allowedHeaders("*"); // Allowed headers
     }
